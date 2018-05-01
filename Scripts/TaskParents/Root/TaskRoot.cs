38e909171b73379bf3c840b6aa8023aaf532e650
Charles Brindle <ash@blueashes.com>
2018-05-01 11:33:54 -0600

    Finished selector base tests, new base composite

---
+++ Scripts/TaskParents/Root/TaskRoot.cs
@@ -0,0 +1,20 @@
+﻿using Adnc.FluidBT.Tasks;
+
+namespace Adnc.FluidBT.TaskParents {
+    public class TaskRoot : TaskParentBase {
+        protected override int MaxChildren { get; } = 1;
+
+        protected override TaskStatus OnUpdate () {
+            if (children.Count == 0) {
+                return TaskStatus.Success;
+            }
+
+            var child = children[0];
+            if (!child.Enabled) {
+                return TaskStatus.Success;
+            }
+
+            return child.Update();
+        }
+    }
+}
